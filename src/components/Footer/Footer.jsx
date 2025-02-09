ee69a3667c39ba7f8de570ef785205b2ad086650
victor <victorlopes9291@gmail.com>
2025-02-09 12:33:11 -0300

    -- add component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+import "./Footer.css";
+
+
+export const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>Feito por <a target={"_blank"} href="https://github.com/CarolLimav">Caroline</a> e <a
+                    href="https://github.com/vlopess" target={"_blank"}>Victor</a></p>
+            </footer>
+        </>
+    );
+}
