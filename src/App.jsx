b6f2d623d251178961b399bfab2fc50e41719540
victor <victorlopes9291@gmail.com>
2025-02-09 12:29:52 -0300

    -- template app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
+
+
+
+
+
 import './App.css'
+import {Card} from "./components/Card/Card.jsx";
+import {Footer} from "./components/Footer/Footer.jsx";
+import BarChart from "./components/BarChart/BarChart.jsx";
+import RadarChart from "./components/RadarChart/RadarChart.jsx";
 
 function App() {
   return (
     <>
-      <div>
+      <div style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw"
+      }}>
+          <Card>
+              <div>
+                  <BarChart/>
+              </div>
+          </Card>
+          <Card>
+              <div>
+                  <RadarChart/>
+              </div>
+          </Card>
+          <Card>
+              <div>
+                  <h1>Test</h1>
+              </div>
+          </Card>
+          <Card>
+              <div>
+                  <h1>Test</h1>
+              </div>
+          </Card>
+          <Card>
+              <div>
+                  <h1>Test</h1>
+              </div>
+          </Card>
       </div>
+    <Footer/>
     </>
   )
 }
