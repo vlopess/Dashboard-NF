17b3f4508632e4325fde4534e5496af5accac00b
victor <victorlopes9291@gmail.com>
2025-02-11 00:01:01 -0300

    -- refactoring footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import "./Footer.css";
 export const Footer = () => {
     return (
         <>
-            <footer>
-                <p>Feito por <a target={"_blank"} href="https://github.com/CarolLimav">Caroline Lima</a> e <a
-                    href="https://github.com/vlopess" target={"_blank"}>Victor Lopes</a></p>
-            </footer>
+            <div className={"stack"}>
+                <img src="https://capsule-render.vercel.app/api?type=waving&height=125&color=0024FF&animation=fadeIn&descAlign=77&fontSize=30&fontColor=FFF&section=footer&reversal=true" alt="footer"/>
+                <p>Feito por <a target={"_blank"} href="https://github.com/CarolLimav">Caroline Lima</a> e <a href="https://github.com/vlopess" target={"_blank"}>Victor Lopes</a></p>
+            </div>
         </>
     );
 }
